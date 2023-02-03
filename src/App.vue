d82fa8c0b810dc388f31f9ef9171e1f50f70a08a
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-02-03 23:38:42 +0700

    feat: finish food page and add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
   <div id="app">
     <router-view />
+    <FooterComponent />
   </div>
 </template>
+
+<script>
+import FooterComponent from "@/components/FooterComponent.vue";
+
+export default {
+  components: {
+    FooterComponent,
+  },
+};
+</script>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="mt-5 mb-5">
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">
+          2023 Copyright - Luthfi Izzuddin Hanif
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style></style>
