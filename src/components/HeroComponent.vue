32461ceaad99d3db3572135a14dcba6fadde22d0
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-02-03 23:29:04 +0700

    feat: add product card component and responsive design

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -1,24 +1,55 @@
 <template>
-  <div class="row mt-4">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2>
-            <strong>Delicious Food Menu, </strong> <br />
-            in Your Gadget
-          </h2>
-          <p>
-            Choose and order your favorite food in
-            <span class="font-primary-color">EatHere</span>!
-          </p>
-          <button class="btn btn-lg btn-success">
-            <b-icon-arrow-right></b-icon-arrow-right> Order
-          </button>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong>
+                <br />in Your Gadget
+              </h2>
+              <p>
+                Choose and order your favorite food in
+                <span class="font-primary-color">EatHere</span>!
+              </p>
+              <router-link class="btn btn-lg btn-success" to="/foods">
+                <b-icon-arrow-right></b-icon-arrow-right> Order
+              </router-link>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/ilustrasi.png" width="100%" />
         </div>
       </div>
     </div>
-    <div class="col">
-      <img src="../assets/images/ilustrasi.png" width="100%" />
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/ilustrasi.png" width="100%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong>
+                <br />in Your Gadget
+              </h2>
+              <p>
+                Choose and order your favorite food in
+                <span class="font-primary-color">EatHere</span>!
+              </p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Order
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,29 @@
     <NavbarComponent />
     <div class="container">
       <HeroComponent />
+      <div class="row mt-4">
+        <div class="col">
+          <h2>
+            Best
+            <strong>Foods</strong>
+          </h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">
+            <b-icon-eye></b-icon-eye> See All
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div
+          class="col-md-4 mt-4"
+          v-for="product in products"
+          :key="product.id"
+        >
+          <CardProduct :product="product" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -11,12 +34,31 @@
 // @ is an alias to /src
 import NavbarComponent from "@/components/NavbarComponent.vue";
 import HeroComponent from "@/components/HeroComponent.vue";
+import CardProduct from "@/components/CardProduct.vue";
+import axios from "axios";
 
 export default {
   name: "HomeView",
   components: {
     NavbarComponent,
     HeroComponent,
+    CardProduct,
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
