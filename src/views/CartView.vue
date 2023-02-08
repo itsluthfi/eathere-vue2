d3774997859b83baadc5e28f0cd0538276f52963
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-02-08 08:17:27 +0700

    feat: add cart page

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,18 +1,204 @@
 <template>
-  <div>
-    <NavbarComponent />
-    <h1>Cart</h1>
+  <div class="cart">
+    <NavbarComponent :updateKeranjang="keranjangs" />
+    <div class="container">
+      <!-- breadcrumb -->
+      <div class="row mt-4">
+        <div class="col">
+          <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item">
+                <router-link to="/" class="text-dark">Home</router-link>
+              </li>
+              <li class="breadcrumb-item">
+                <router-link to="/foods" class="text-dark">Foods</router-link>
+              </li>
+              <li class="breadcrumb-item active" aria-current="page">Cart</li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <h2>
+            My
+            <strong>Cart</strong>
+          </h2>
+          <div class="table-responsive mt-3">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Image</th>
+                  <th scope="col">Food</th>
+                  <th scope="col">Detail</th>
+                  <th scope="col">Qty</th>
+                  <th scope="col">Price</th>
+                  <th scope="col">Total Price</th>
+                  <th scope="col">Delete</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(keranjang, index) in keranjangs"
+                  :key="keranjang.id"
+                >
+                  <th>{{ index + 1 }}</th>
+                  <td>
+                    <img
+                      :src="
+                        require(`../assets/images/${keranjang.products.gambar}`)
+                      "
+                      class="img-fluid shadow"
+                      width="250"
+                    />
+                  </td>
+                  <td>
+                    <strong>{{ keranjang.products.nama }}</strong>
+                  </td>
+                  <td>
+                    {{ keranjang.keterangan ? keranjang.keterangan : "-" }}
+                  </td>
+                  <td>{{ keranjang.jumlah_pemesanan }}</td>
+                  <td align="right">Rp. {{ keranjang.products.harga }}</td>
+                  <td align="right">
+                    <strong
+                      >Rp.
+                      {{
+                        keranjang.products.harga * keranjang.jumlah_pemesanan
+                      }}</strong
+                    >
+                  </td>
+                  <td align="center" class="text-danger">
+                    <b-icon-trash
+                      @click="hapusKeranjang(keranjang.id)"
+                    ></b-icon-trash>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td colspan="6" align="right">
+                    <strong>Total Price:</strong>
+                  </td>
+                  <td align="right">
+                    <strong>Rp. {{ totalHarga }}</strong>
+                  </td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <!-- Form Checkout -->
+      <div class="row justify-content-end">
+        <div class="col-md-4">
+          <form class="mt-4" v-on:submit.prevent>
+            <div class="form-group">
+              <label for="nama">Name:</label>
+              <input type="text" class="form-control" v-model="pesan.nama" />
+            </div>
+            <div class="form-group">
+              <label for="noMeja">Table Number:</label>
+              <input type="text" class="form-control" v-model="pesan.noMeja" />
+            </div>
+
+            <button
+              type="submit"
+              class="btn btn-success float-right"
+              @click="checkout"
+            >
+              <b-icon-cart></b-icon-cart> Order
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import NavbarComponent from "@/components/NavbarComponent.vue";
-
+import axios from "axios";
 export default {
   name: "CartView",
   components: {
     NavbarComponent,
+  },
+  data() {
+    return {
+      keranjangs: [],
+      pesan: {},
+    };
+  },
+  methods: {
+    setKeranjangs(data) {
+      this.keranjangs = data;
+    },
+    hapusKeranjang(id) {
+      axios
+        .delete("http://localhost:3000/keranjangs/" + id)
+        .then(() => {
+          this.$toast.error("Success delete cart!", {
+            type: "error",
+            position: "top-right",
+            duration: 3000,
+            dismissible: true,
+          });
+          // Update Data keranjang
+          axios
+            .get("http://localhost:3000/keranjangs")
+            .then((response) => this.setKeranjangs(response.data))
+            .catch((error) => console.log(error));
+        })
+        .catch((error) => console.log(error));
+    },
+    checkout() {
+      if (this.pesan.nama && this.pesan.noMeja) {
+        this.pesan.keranjangs = this.keranjangs;
+        axios
+          .post("http://localhost:3000/pesanans", this.pesan)
+          .then(() => {
+            // Hapus Semua Keranjang
+            this.keranjangs.map(function (item) {
+              return axios
+                .delete("http://localhost:3000/keranjangs/" + item.id)
+                .catch((error) => console.log(error));
+            });
+            this.$router.push({ path: "/success-order" });
+            this.$toast.success("Success order!", {
+              type: "success",
+              position: "top-right",
+              duration: 3000,
+              dismissible: true,
+            });
+          })
+          .catch((err) => console.log(err));
+      } else {
+        this.$toast.error("Name and table number must be filled!", {
+          type: "error",
+          position: "top-right",
+          duration: 3000,
+          dismissible: true,
+        });
+      }
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/keranjangs")
+      .then((response) => this.setKeranjangs(response.data))
+      .catch((error) => console.log(error));
+  },
+  computed: {
+    totalHarga() {
+      return this.keranjangs.reduce(function (items, data) {
+        return items + data.products.harga * data.jumlah_pemesanan;
+      }, 0);
+    },
   },
 };
 </script>
